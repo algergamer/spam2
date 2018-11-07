3af87f24e788238c3de4be48adff9ace02928821
algergamer <43455217+algergamer@users.noreply.github.com>
2018-11-07 22:01:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('ready', async() => {
 var server = "509440408145625088"; // ايدي السررفر
-var channel = "509829108159741953";//ايدي الروم
+var channel = "509834778812612648";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
